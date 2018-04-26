838340ae9bdf4803076e861a58e13e00f8f2b10b
JessDF <jdowding@csumb.edu>
2018-04-26 13:58:29 -0700

    Create initial routes for react components, however not routing to avg-donations yet

---
+++ webapp/components/AvgDonations.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AvgDonations extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Average Donation per Win </h1>
+            </div>
+        );
+    }
+}
+
+export default AvgDonations;
--- webapp/index.jsx
+++ webapp/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, Switch } from 'react-router';
+
 import App from './components/App';
+import AvgDonations from './components/AvgDonations';
+
+import createBrowserHistory from 'history/createBrowserHistory'
+const newHistory = createBrowserHistory();
 
 const node = document.querySelector('#app');
-render(<App />, node);
+
+let routes = (
+    <Router history={newHistory}>
+        <Switch>
+           <Route exact path="/" component={App} />
+           <Route path="/avg-donations" component={AvgDonations} />
+        </Switch>
+     </Router>
+);
+render(routes, node);
