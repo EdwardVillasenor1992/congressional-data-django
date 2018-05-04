2678c0af399b7134c2b5173f08e6e0832840e940
EdwardVillasenor1992 <35983571+EdwardVillasenor1992@users.noreply.github.com>
2018-05-04 15:18:54 -0700

    creating simple navbar
    creating a simple navbar that is integrated in index.jsx

--- webapp/index.jsx
+++ webapp/index.jsx
@@ -4,17 +4,22 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import App from './components/App';
 import AvgDonations from './components/AvgDonations';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
 
 const node = document.querySelector('#app');
 
 const routes = (
-    <Router>
-        <div>
-            <Link to="/">Main Page | </Link>
-            <Link to="/average">Average Donation Per Win</Link>
-            <Route exact path="/" component={App} />
-            <Route path="/average" component={AvgDonations} />
-        </div>
-    </Router>
+<Router>
+  <Navbar>
+    <Nav>
+        <NavItem eventKey={1} componentClass={Link} href="/" to="/"> Main Page </NavItem>
+        <NavItem eventKey={2} componentClass={Link} href="/average" to="/average">Average Donation Per Win</NavItem>
+<br />
+        <Route exact path="/" component={App} />
+        <Route path="/average" component={AvgDonations} />
+    </Nav>
+  </Navbar>
+</Router>
+
 );
 render(routes, node);
